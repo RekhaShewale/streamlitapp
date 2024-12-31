a76c63945763d12f18f8143f79e5d521225262ac
Rekha Shewale <rekhashewale94@gmail.com>
2024-12-31 21:46:21 +0530

    jenkins file updated

--- etl.py
+++ etl.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.write("AVD2")
+st.write("AVD1")
 data ={
 	 	'Task' : ['Extract', 'Transform' , 'Load'],
 		'Status' : ['Completed' , 'InProgress' , 'Pending']
